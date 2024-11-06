f95b76120890a09a8190c8eadd0017243d1ced3c
Olya <lezimveos@gmail.com>
2024-11-06 10:50:59 +0300

    БД

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,21 +1,21 @@
 package jm.task.core.jdbc;
-import jm.task.core.jdbc.model.User;
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.SQLException;
 
-import static jm.task.core.jdbc.service.UserService.userDao;
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+
+import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException, IllegalAccessException {
-        User user = new User();
-        userDao.createUsersTable();
-        userDao.saveUser("Jane", "Doe", 25);
-        userDao.saveUser("John", "Doe", 25);
-        userDao.removeUserById(1);
+        UserDao userDao = new UserDaoHibernateImpl();
+        UserService userService = new UserServiceImpl(userDao);
+
+        userService.createUsersTable();
+        userService.saveUser("Jane", "Doe", 25);
+        userService.saveUser("John", "Doe", 25);
+        userService.removeUserById(1);
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserService.java
+++ src/main/java/jm/task/core/jdbc/service/UserService.java
@@ -1,15 +1,11 @@
 package jm.task.core.jdbc.service;
 
-import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.sql.SQLException;
 import java.util.List;
 
 public interface UserService {
-    UserDao userDao = new UserDaoHibernateImpl();
-
     void createUsersTable() throws SQLException;
 
     void dropUsersTable() throws SQLException;
